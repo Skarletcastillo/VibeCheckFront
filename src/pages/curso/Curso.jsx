65c2f57f13163c50b68f33d10bd386ca05adf923
Skarletcastillo <genessisskarlet@gmail.com>
2025-06-11 20:58:52 -0500

    Implementación de estilos

--- src/pages/curso/Curso.jsx
+++ src/pages/curso/Curso.jsx
@@ -1,14 +1,37 @@
+import "./Curso.css";
+
 const Cursos = () => {
   const usuario = JSON.parse(localStorage.getItem("usuario"));
 
   if (!usuario) return <p>No autorizado</p>;
 
+  // Vista Admin
   if (usuario.rol === "admin") {
-    return <p>Vista de cursos para administrador</p>;
+    return (
+      <div className="ContainerViewAdmin">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis,
+        </p>
+      </div>
+    );
+
+    // Vista Profesor
   } else if (usuario.rol === "profesor") {
-    return <p>Vista de cursos para profesor</p>;
+    return (
+      <div className="ContainerViewProfesor">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis,
+        </p>
+      </div>
+    );
+
+    // Vista Alumno
   } else {
-    return <p>Vista de cursos para alumno</p>;
+    <div className="ContainerViewAlumno">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, id!
+      </p>
+    </div>;
   }
 };
 
